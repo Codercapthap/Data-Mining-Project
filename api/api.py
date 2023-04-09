3e4743c2ce7774cfe700bfe019f04c33796091d5
keqing <khiemb1910393@student.ctu.edu.vn>
2023-04-09 17:39:51 +0700

    change variable name, update comment

--- api/api.py
+++ api/api.py
@@ -1,11 +1,6 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import preprocessing
-import seaborn as sns
-import matplotlib.pyplot as plt
-from sklearn import metrics
-from sklearn.model_selection import train_test_split
 import pandas as pd
-import numpy as np
 import flask
 from flask import request
 
@@ -18,82 +13,67 @@ app.config["DEBUG"] = True
 data_path = "bank-full.csv"  # load dataset
 data = pd.DataFrame(pd.read_csv(data_path, delimiter=";"))
 
-# job
-# marital
-# education
-# default (binary)
-# housing (binary)
-# loan (binary)
-# contact
-# month
-# poutcome
-
-ohejob = preprocessing.OneHotEncoder()
-X = ohejob.fit_transform(data.job.values.reshape(-1, 1)).toarray()
+"""# 2. Xử lí dữ liệu"""
+# chia các dữ liệu chữ thành các cột
+OHE = preprocessing.OneHotEncoder()
+X = OHE.fit_transform(data.job.values.reshape(-1, 1)).toarray()
 OHEjob = pd.DataFrame(X, columns=["admin.", "unknown", "unemployed", "management", "housemaid", "entrepreneur", "student",
                                   "blue-collar", "self-employed", "retired", "technician", "services"])
 data = pd.concat([data, OHEjob], axis=1)
 data = data.drop("job", axis=1)
 
-ohemarital = preprocessing.OneHotEncoder()
-X = ohemarital.fit_transform(data.marital.values.reshape(-1, 1)).toarray()
+X = OHE.fit_transform(data.marital.values.reshape(-1, 1)).toarray()
 OHEmarital = pd.DataFrame(X, columns=["married", "divorced", "single"])
 data = pd.concat([data, OHEmarital], axis=1)
 data = data.drop("marital", axis=1)
 
-oheeducation = preprocessing.OneHotEncoder()
-X = oheeducation.fit_transform(data.education.values.reshape(-1, 1)).toarray()
+X = OHE.fit_transform(data.education.values.reshape(-1, 1)).toarray()
 OHEeducation = pd.DataFrame(
     X, columns=["unknown", "secondary", "primary", "tertiary"])
 data = pd.concat([data, OHEeducation], axis=1)
 data = data.drop("education", axis=1)
 
-ohecontact = preprocessing.OneHotEncoder()
-X = ohecontact.fit_transform(data.contact.values.reshape(-1, 1)).toarray()
+X = OHE.fit_transform(data.contact.values.reshape(-1, 1)).toarray()
 OHEcontact = pd.DataFrame(X, columns=["unknown", "telephone", "cellular"])
 data = pd.concat([data, OHEcontact], axis=1)
 data = data.drop("contact", axis=1)
 
-ohemonth = preprocessing.OneHotEncoder()
-X = ohemonth.fit_transform(data.month.values.reshape(-1, 1)).toarray()
+X = OHE.fit_transform(data.month.values.reshape(-1, 1)).toarray()
 OHEmonth = pd.DataFrame(X, columns=[
                         "jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"])
 data = pd.concat([data, OHEmonth], axis=1)
 data = data.drop("month", axis=1)
 
-ohepoutcome = preprocessing.OneHotEncoder()
-X = ohepoutcome.fit_transform(data.poutcome.values.reshape(-1, 1)).toarray()
+X = OHE.fit_transform(data.poutcome.values.reshape(-1, 1)).toarray()
 OHEpoutcome = pd.DataFrame(
     X, columns=["unknown", "other", "failure", "success"])
 data = pd.concat([data, OHEpoutcome], axis=1)
 data = data.drop("poutcome", axis=1)
 
-ledefault = preprocessing.LabelEncoder()
-data["default"] = ledefault.fit_transform(data['default'])
+# dữ liệu yes no số hóa thành 1 và 0
+labelEncode = preprocessing.LabelEncoder()
+data["default"] = labelEncode.fit_transform(data['default'])
 
-lehousing = preprocessing.LabelEncoder()
-data["housing"] = lehousing.fit_transform(data['housing'])
+data["housing"] = labelEncode.fit_transform(data['housing'])
 
-leloan = preprocessing.LabelEncoder()
-data["loan"] = leloan.fit_transform(data['loan'])
+data["loan"] = labelEncode.fit_transform(data['loan'])
 
+data["y"] = labelEncode.fit_transform(data['y'])
 
-ley = preprocessing.LabelEncoder()
-data["y"] = ley.fit_transform(data['y'])
+# bỏ đi cột unknown vì không có giá trị thực tế
 data = data.drop("unknown", axis=1)
 
+# chia dữ liệu thành tập thuộc tính và tập nhãn
 X = data.iloc[:, data.columns != 'y']
 y = data['y']
-# X_train, X_test, y_train, y_test = train_test_split(
-#     X, y, test_size=0.2, random_state=0)
 
+"""# 3. Huấn luyện mô hình"""
 model = RandomForestClassifier(n_estimators=100)
 model.fit(X, y)
 
 
 @app.route('/api/getResult', methods=['POST'])
 def api_all():
-    print(request.json)
     content = request.json
     age = content['age']
     job = content['job']
@@ -112,7 +92,8 @@ def api_all():
     previous = content['previous']
     poutcome = content['poutcome']
 
-    df = pd.DataFrame({
+    """# Xử lí dữ liệu input"""
+    dfInput = pd.DataFrame({
         "age": [age, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         "job": [job, "admin.", "unknown", "unemployed", "management", "housemaid", "entrepreneur", "student",
                 "blue-collar", "self-employed", "retired", "technician", "services"],
@@ -132,57 +113,50 @@ def api_all():
         "poutcome": [poutcome, "unknown", "other", "failure", "success", "unknown", "other", "failure", "success", "unknown", "other", "failure", "success"]
     })
 
-    ohejob = preprocessing.OneHotEncoder()
-    X = ohejob.fit_transform(df.job.values.reshape(-1, 1)).toarray()
+    X = OHE.fit_transform(dfInput.job.values.reshape(-1, 1)).toarray()
     OHEjob = pd.DataFrame(X, columns=["admin.", "unknown", "unemployed", "management", "housemaid", "entrepreneur", "student",
                                       "blue-collar", "self-employed", "retired", "technician", "services"])
-    df = pd.concat([df, OHEjob], axis=1)
-    df = df.drop("job", axis=1)
+    dfInput = pd.concat([dfInput, OHEjob], axis=1)
+    dfInput = dfInput.drop("job", axis=1)
 
-    ohemarital = preprocessing.OneHotEncoder()
-    X = ohemarital.fit_transform(df.marital.values.reshape(-1, 1)).toarray()
+    X = OHE.fit_transform(dfInput.marital.values.reshape(-1, 1)).toarray()
     OHEmarital = pd.DataFrame(X, columns=["married", "divorced", "single"])
-    df = pd.concat([df, OHEmarital], axis=1)
-    df = df.drop("marital", axis=1)
+    dfInput = pd.concat([dfInput, OHEmarital], axis=1)
+    dfInput = dfInput.drop("marital", axis=1)
 
-    oheeducation = preprocessing.OneHotEncoder()
-    X = oheeducation.fit_transform(
-        df.education.values.reshape(-1, 1)).toarray()
+    X = OHE.fit_transform(
+        dfInput.education.values.reshape(-1, 1)).toarray()
     OHEeducation = pd.DataFrame(
         X, columns=["unknown", "secondary", "primary", "tertiary"])
-    df = pd.concat([df, OHEeducation], axis=1)
-    df = df.drop("education", axis=1)
+    dfInput = pd.concat([dfInput, OHEeducation], axis=1)
+    dfInput = dfInput.drop("education", axis=1)
 
-    ohecontact = preprocessing.OneHotEncoder()
-    X = ohecontact.fit_transform(df.contact.values.reshape(-1, 1)).toarray()
+    X = OHE.fit_transform(dfInput.contact.values.reshape(-1, 1)).toarray()
     OHEcontact = pd.DataFrame(X, columns=["unknown", "telephone", "cellular"])
-    df = pd.concat([df, OHEcontact], axis=1)
-    df = df.drop("contact", axis=1)
+    dfInput = pd.concat([dfInput, OHEcontact], axis=1)
+    dfInput = dfInput.drop("contact", axis=1)
 
-    ohemonth = preprocessing.OneHotEncoder()
-    X = ohemonth.fit_transform(df.month.values.reshape(-1, 1)).toarray()
+    X = OHE.fit_transform(dfInput.month.values.reshape(-1, 1)).toarray()
     OHEmonth = pd.DataFrame(X, columns=[
                             "jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"])
-    df = pd.concat([df, OHEmonth], axis=1)
-    df = df.drop("month", axis=1)
+    dfInput = pd.concat([dfInput, OHEmonth], axis=1)
+    dfInput = dfInput.drop("month", axis=1)
 
-    ohepoutcome = preprocessing.OneHotEncoder()
-    X = ohepoutcome.fit_transform(df.poutcome.values.reshape(-1, 1)).toarray()
+    X = OHE.fit_transform(dfInput.poutcome.values.reshape(-1, 1)).toarray()
     OHEpoutcome = pd.DataFrame(
         X, columns=["unknown", "other", "failure", "success"])
-    df = pd.concat([df, OHEpoutcome], axis=1)
-    df = df.drop("poutcome", axis=1)
-    ledefault = preprocessing.LabelEncoder()
-    df["default"] = ledefault.fit_transform(df['default'])
+    dfInput = pd.concat([dfInput, OHEpoutcome], axis=1)
+    dfInput = dfInput.drop("poutcome", axis=1)
 
-    lehousing = preprocessing.LabelEncoder()
-    df["housing"] = lehousing.fit_transform(df['housing'])
+    dfInput["default"] = labelEncode.fit_transform(dfInput['default'])
 
-    leloan = preprocessing.LabelEncoder()
-    df["loan"] = leloan.fit_transform(df['loan'])
-    df = df.drop("unknown", axis=1)
+    dfInput["housing"] = labelEncode.fit_transform(dfInput['housing'])
 
-    prediction = model.predict(df.head(1))
+    dfInput["loan"] = labelEncode.fit_transform(dfInput['loan'])
+    dfInput = dfInput.drop("unknown", axis=1)
+
+    """# Dự đoán bằng mô hình và trả về"""
+    prediction = model.predict(dfInput.head(1))
     result = prediction.tolist()
     return flask.jsonify(result[0])
 
